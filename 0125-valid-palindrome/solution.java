9a7c21ce6d49f9831130757f979219abe4c3df74
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2025-10-08 04:47:25 +0000

    Sync LeetCode submission Runtime - 4 ms (49.29%), Memory - 43.7 MB (99.06%)

---
+++ 0125-valid-palindrome/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+       s=s.toLowerCase();
+
+       StringBuilder sb=new StringBuilder();
+
+       for(char c:s.toCharArray()){
+        if(Character.isLetterOrDigit(c)){
+            sb.append(c);
+        }
+       }
+       int left=0,right=sb.length()-1;
+
+       while(left<right){
+        if(sb.charAt(left)!=sb.charAt(right)){
+            return false;
+        }
+        left++;
+        right--;
+       }
+       return true;
+    }
+}
