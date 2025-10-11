cdc9e42b2bfb898bcc8ba30784dd1bf9b9112f0e
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2025-10-11 13:29:13 +0000

    Sync LeetCode submission Runtime - 306 ms (100.00%), Memory - 0.0B (100.00%)

---
+++ 0570-managers-with-at-least-5-direct-reports/solution.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+
+select e.name from Employee e where e.id in(select managerId from Employee where managerId is not null group by managerId having count(id)>=5);
