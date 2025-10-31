02f67c6973c11d922fb874e1598f5688fe6fe2e7
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2025-10-31 14:33:14 +0000

    Sync LeetCode submission Runtime - 397 ms (100.00%), Memory - 0.0B (100.00%)

---
+++ 0608-tree-node/solution.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT 
+    id,
+    CASE
+        WHEN p_id IS NULL THEN 'Root'
+        WHEN id IN (SELECT DISTINCT p_id FROM Tree WHERE p_id IS NOT NULL) THEN 'Inner'
+        ELSE 'Leaf'
+    END AS Type
+FROM Tree
+ORDER BY id;
+
