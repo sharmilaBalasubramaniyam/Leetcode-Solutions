90ee59c8e5755f600b26d18cd761a258818a5de4
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2025-11-05 13:24:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.8 MB (70.68%)

---
+++ 1329-minimum-cost-to-move-chips-to-the-same-position/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int e=0,o=0;
+        for(int m:position){
+            if(m%2==0){
+                e++;
+            }else{
+                o++;
+            }  
+        }
+        return Math.min(e,o);
+    }
+}
