b3712c73dccdc6ff2d3dcf27dde8e38c7a0d6bc6
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2025-11-05 14:44:59 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 44 MB (25.85%)

--- 4052-equal-score-substrings/solution.java
+++ 4052-equal-score-substrings/solution.java
@@ -1,18 +1,14 @@
 class Solution {
     public boolean scoreBalance(String s) {
-        int n=s.length();
-        int[] p=new int[n];
-        p[0]=s.charAt(0)-'a'+1;
+        int total = 0;
+        for (char c : s.toCharArray())
+            total += c - 'a' + 1;
 
-        for(int i=1;i<n;i++){
-            p[i]=p[i-1]+(s.charAt(i)-'a'+1);
-        }
-            for(int i=1;i<n;i++){
-            int l=p[i-1];
-            int r=p[n-1]-p[i-1];
-            if(l==r){
+        int left = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            left += s.charAt(i) - 'a' + 1;
+            if (left == total - left)
                 return true;
-            }
         }
         return false;
     }
