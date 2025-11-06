cdaf980f569adf3a702ecb13097a130bb71ad7fc
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2025-11-06 00:35:37 +0000

    Sync LeetCode submission Runtime - 3 ms (31.97%), Memory - 42.6 MB (79.76%)

--- 4051-remove-zeros-in-decimal-representation/solution.java
+++ 4051-remove-zeros-in-decimal-representation/solution.java
@@ -1,10 +1,20 @@
 class Solution {
     public long removeZeros(long n) {
-        
         String s=Long.toString(n).replace("0","");
         return Long.parseLong(s);
-        
-    
+
+
+        // String s=Long.toString(n);
+        // String r="";
+
+        // for(int i=0;i<s.length();i++){
+        //     if(s.charAt(i)=='0'){
+        //         continue;
+        //     }
+        //     r+=s.charAt(i);
+        // }
+        // return Long.parseLong(r);
     }
+
     
 }
