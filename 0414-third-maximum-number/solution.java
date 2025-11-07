15501e79ddf8db5f23a038fb9d17260b55f9f1f1
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2025-11-07 13:23:49 +0000

    Sync LeetCode submission Runtime - 3 ms (75.20%), Memory - 44.8 MB (60.63%)

--- 0414-third-maximum-number/solution.java
+++ 0414-third-maximum-number/solution.java
@@ -1,25 +1,26 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        if(nums.length<=0) return -1;
-
-        long f=Long.MIN_VALUE;
-        long s=Long.MIN_VALUE;
-        long t=Long.MIN_VALUE;
-
-        for(int num:nums){
-            if(num==f||num==s||num==t)
+       Long first = null, second = null, third = null;
+        
+        for (int n : nums) {
+            long num = n;
+            if ((first != null && num == first) ||
+                (second != null && num == second) ||
+                (third != null && num == third))
                 continue;
-            if(num>f){
-                t=s;
-                s=f;
-                f=num;
-            }else if(num>s){
-                t=s;
-                s=num;
-            }else if(num>t){
-                t=num;
+            
+            if (first == null || num > first) {
+                third = second;
+                second = first;
+                first = num;
+            } else if (second == null || num > second) {
+                third = second;
+                second = num;
+            } else if (third == null || num > third) {
+                third = num;
             }
         }
-        return (t==Long.MIN_VALUE)?(int)f:(int)t;
+        
+        return (third == null) ? first.intValue() : third.intValue();
     }
 }
