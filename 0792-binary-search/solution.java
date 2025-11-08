da4beebbaa0d8ab137aa03ac285ccd20a473a306
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2025-11-08 06:12:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 48.2 MB (68.45%)

---
+++ 0792-binary-search/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n=nums.length;
+        int l=0;
+        int h=n-1;
+
+        while(l<=h){
+            int mid=l+(h-l)/2;
+
+            if(nums[mid]==target){
+                return mid;
+            }else if(target<nums[mid]){
+                h=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        return -1;
+    }
+}
