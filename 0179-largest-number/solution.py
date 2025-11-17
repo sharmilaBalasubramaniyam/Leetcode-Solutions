7dfa59e24f1aa4f3bc3d62e25f5cd1e27adcc066
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2025-11-17 13:28:58 +0000

    Sync LeetCode submission Runtime - 4 ms (31.81%), Memory - 17.9 MB (100.00%)

---
+++ 0179-largest-number/solution.py
@@ -0,0 +1,5 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums=list(map(str,nums))
+        nums=(sorted(nums,key=lambda x:x*10,reverse=True))
+        return "0" if nums[0]=="0" else "".join(nums)
