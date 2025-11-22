d9e1cc42bc76a6de2a67c70c4eb02cfb073854bd
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2025-11-22 15:24:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.9 MB (100.00%)

---
+++ 0717-1-bit-and-2-bit-characters/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        # i=0
+        # n=len(bits)
+        # while i<n-1:
+        #     if bits[i]==1:
+        #         i+=2
+        #     else:
+        #         i+=1
+        # return i==n-1       
+
+        c=0
+        i=len(bits)-2
+
+        while i>=0 and bits[i]==1:
+            c+=1
+            i-=1
+        return c%2==0
