4c16bef5ffbee925ecdccbbb0592e837ba10786a
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2025-12-04 14:04:16 +0000

    Sync LeetCode submission Runtime - 344 ms (5.64%), Memory - 68 MB (5.08%)

---
+++ 1908-recyclable-and-low-fat-products/solution.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    res=products[(products['low_fats']=='Y') & (products['recyclable']=='Y')]
+    return res[['product_id']]
