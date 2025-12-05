912b54f16eee20f299dd12267111e509b974729b
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2025-12-05 13:00:46 +0000

    Sync LeetCode submission Runtime - 502 ms (99.77%), Memory - 0.0B (100.00%)

---
+++ 1942-primary-department-for-each-employee/solution.sql
@@ -0,0 +1,3 @@
+select employee_id,department_id from Employee where primary_flag="Y"
+union
+select employee_id,department_id from Employee where employee_id in (select employee_id from Employee group by employee_id having sum(primary_flag="Y")=0);
