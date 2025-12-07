b57b104b040aef2183c48a17d057ce6382136a3c
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2025-12-07 02:58:05 +0000

    Sync LeetCode submission Runtime - 218 ms (100.00%), Memory - 0.0B (100.00%)

--- 0620-not-boring-movies/solution.sql
+++ 0620-not-boring-movies/solution.sql
@@ -1,2 +1,3 @@
 # Write your MySQL query statement below
-select * from Cinema where id%2=1 and description!="boring" order by rating desc;
+
+select * from Cinema where description <> 'boring' and id%2=1 order by rating desc;
