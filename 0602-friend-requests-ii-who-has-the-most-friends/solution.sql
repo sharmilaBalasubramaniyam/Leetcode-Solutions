107604737f2b824e24f405960ef675a65bf49d97
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2025-12-07 13:11:19 +0000

    Sync LeetCode submission Runtime - 280 ms (99.93%), Memory - 0.0B (100.00%)

--- 0602-friend-requests-ii-who-has-the-most-friends/solution.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/solution.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+# Write your MySQL query statement below
 SELECT id, COUNT(*) AS num
 FROM (
     SELECT requester_id AS id FROM RequestAccepted
@@ -8,4 +9,3 @@ FROM (
 GROUP BY id
 ORDER BY num DESC
 LIMIT 1;
-
