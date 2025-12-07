ca22843b2772272e61061223c3b6074f553c9d72
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2025-12-07 13:01:29 +0000

    Sync LeetCode submission Runtime - 736 ms (70.88%), Memory - 0.0B (100.00%)

---
+++ 1462-list-the-products-ordered-in-a-period/solution.sql
@@ -0,0 +1,1 @@
+select p.product_name,sum(o.unit) as unit from Products p left join Orders o on p.product_id=o.product_id where o.order_date between '2020-02-01' and '2020-02-29' group by p.product_id having sum(o.unit)>=100; 
