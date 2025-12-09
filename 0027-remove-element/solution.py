e83a2c50879cb369daed66ad5c11da8ff9d56d79
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2025-12-09 15:29:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.9 MB (100.00%)

---
+++ 0027-remove-element/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        c=0
+        for num in nums:
+            if num!=val:
+                nums[c]=num
+                c+=1
+        return c
