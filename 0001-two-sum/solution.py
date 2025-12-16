540aedcebfc54ff9ee834d1590f90187ecb88ba4
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2025-12-16 09:58:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.9 MB (100.00%)

---
+++ 0001-two-sum/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen={}
+        for i,num in enumerate(nums):
+            c=target-num
+
+            if c in seen:
+                return [seen[c],i]
+            
+            seen[num]=i
