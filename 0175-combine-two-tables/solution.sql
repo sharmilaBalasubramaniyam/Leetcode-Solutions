ac38bbe5b105e0da0ce193b4f6107adb849ba5df
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2025-12-20 06:49:41 +0000

    Sync LeetCode submission Runtime - 360 ms (99.99%), Memory - 0.0B (100.00%)

--- 0175-combine-two-tables/solution.sql
+++ 0175-combine-two-tables/solution.sql
@@ -1,3 +1,1 @@
-select p.firstname,p.lastname,a.city,a.state from person p 
-left join address a 
-on p.personId=a.personId;
+select p.firstName,p.lastName,a.city,a.state from Person p left join Address a on p.personId=a.personId;
