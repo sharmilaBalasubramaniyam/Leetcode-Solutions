d66c37d930d60cbfeb640351a913418004ba6719
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2025-12-31 13:36:53 +0000

    Sync LeetCode submission Runtime - 495 ms (62.11%), Memory - 0.0B (100.00%)

--- 0197-rising-temperature/solution.sql
+++ 0197-rising-temperature/solution.sql
@@ -1,3 +1,1 @@
-# Write your MySQL query statement below
-
-select w1.id from Weather w1 join Weather w2 on date_sub(w1.recordDate,interval 1 day)=w2.recordDate where w1.temperature>w2.temperature;
+select w1.id from weather w1 join weather w2 on datediff(w1.recordDate,w2.recordDate)=1 where w1.temperature>w2.temperature;
