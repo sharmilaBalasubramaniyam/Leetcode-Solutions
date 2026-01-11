6a4e06c4f779dc3d79531815a76655a693ac936e
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2026-01-11 02:47:50 +0000

    Sync LeetCode submission Runtime - 7 ms (25.50%), Memory - 19.5 MB (16.23%)

---
+++ 4186-count-residue-prefixes/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def residuePrefixes(self, s: str) -> int:
+        seen=set()
+        c=0
+        for i in range(len(s)):
+            seen.add(s[i])
+            if len(seen)==(i+1)%3:
+                c+=1
+        return c
