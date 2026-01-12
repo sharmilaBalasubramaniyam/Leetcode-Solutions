e4f36013ec92fed4b74d0edf2e58378c35c41fdf
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2026-01-12 14:25:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.5 MB (18.57%)

--- 0066-plus-one/solution.py
+++ 0066-plus-one/solution.py
@@ -4,6 +4,15 @@ class Solution:
         #num += 1
         #return list(map(int, str(num)))
 
+        #n=len(digits)
+
+        #for i in range(n-1,-1,-1):
+        #    if digits[i]<9:
+        #        digits[i]+=1
+        #        return digits
+        #    digits[i]=0
+        #return [1]+digits
+
         n=len(digits)
 
         for i in range(n-1,-1,-1):
