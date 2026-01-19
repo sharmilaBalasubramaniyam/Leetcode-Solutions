b5c14edf85968fdcbb162699646ab2feb3086f3e
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2026-01-19 13:07:04 +0000

    Sync LeetCode submission Runtime - 3 ms (53.74%), Memory - 20.4 MB (41.43%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -11,11 +11,21 @@ class Solution:
                 
         #        seen[num]=i
 
-        n=len(nums)
+        #n=len(nums)
 
-        for i in range(n):
-            for j in range(i+1,n):
-                if nums[i]+nums[j]==target:
-                    return [i,j]
+        #for i in range(n):
+        #    for j in range(i+1,n):
+        #        if nums[i]+nums[j]==target:
+        #           return [i,j]
+                    
+        seen={}
+
+        for i,num in enumerate(nums):
+            c=target-num
+
+            if c in seen:
+                return [seen[c],i]
+            seen[num]=i
+        
             
         
