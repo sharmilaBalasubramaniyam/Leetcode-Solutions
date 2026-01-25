0bf880998735681e0ea0e4e80d169381bd06ec49
sharmilaBalasubramaniyam <152759314+sharmilaBalasubramaniyam@users.noreply.github.com>
2026-01-25 02:32:17 +0000

    Sync LeetCode submission Runtime - 7 ms (96.23%), Memory - 34.6 MB (40.38%)

---
+++ 4199-minimum-prefix-removal-to-make-array-strictly-increasing/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimumPrefixLength(self, nums: List[int]) -> int:
+        n=len(nums)
+        i=n-1
+        while i>0 and nums[i-1]<nums[i]:
+            i-=1
+        return i
+        
